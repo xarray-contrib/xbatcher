8ffec205ea75c57e8a21aeb043414219bfbd52aa
Max Jones <meghanj@alum.mit.edu>
2022-05-10 14:36:16 -0400

    Use pangeo_sphinx_book_theme

--- doc/conf.py
+++ doc/conf.py
@@ -14,7 +14,6 @@
 
 # type: ignore
 
-import os
 import sys
 
 import sphinx_autosummary_accessors
@@ -148,18 +147,10 @@ pygments_style = 'sphinx'
 
 # -- Options for HTML output ----------------------------------------------
 
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 # tml_theme = 'default'
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'pangeo_sphinx_book_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
