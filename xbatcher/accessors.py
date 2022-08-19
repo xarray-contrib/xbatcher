d6865a2cbaecf49c5898479d4710d0d34c0238f7
Wei Ji <23487320+weiji14@users.noreply.github.com>
2022-08-19 13:18:59 -0400

    Let torch accessor support xarray.Dataset objects
    Convert xarray.Dataset to xarray.DataArray first, so that the `.data` method work to get the underlying array which can be converted to a torch.Tensor.

--- xbatcher/accessors.py
+++ xbatcher/accessors.py
@@ -27,15 +27,32 @@ class BatchAccessor:
 
 
 @xr.register_dataarray_accessor('torch')
+@xr.register_dataset_accessor('torch')
 class TorchAccessor:
     def __init__(self, xarray_obj):
         self._obj = xarray_obj
+
+    def _as_xarray_dataarray(self, xr_obj):
+        """
+        Convert xarray.Dataset to xarray.DataArray if needed, so that it can
+        be converted into a torch.Tensor object.
+        """
+        try:
+            # Convert xr.Dataset to xr.DataArray
+            dataarray = xr_obj.to_array()
+        except AttributeError:  # 'DataArray' object has no attribute 'to_array'
+            # If object is already an xr.DataArray
+            dataarray = xr_obj
+
+        return dataarray
 
     def to_tensor(self):
         """Convert this DataArray to a torch.Tensor"""
         import torch
 
-        return torch.tensor(self._obj.data)
+        dataarray = self._as_xarray_dataarray(self._obj)
+
+        return torch.tensor(data=dataarray.data)
 
     def to_named_tensor(self):
         """
@@ -45,4 +62,6 @@ class TorchAccessor:
         """
         import torch
 
-        return torch.tensor(self._obj.data, names=tuple(self._obj.sizes))
+        dataarray = self._as_xarray_dataarray(self._obj)
+
+        return torch.tensor(data=dataarray.data, names=tuple(dataarray.sizes))
