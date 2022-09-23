9b9aa386f6bbee252f6fc9fd38936866500fcbc4
Raphael Hagen <norlandrhagen@gmail.com>
2022-09-23 12:54:52 -0700

    issue #89. Use importlib.metadata rather than pkg_resources

--- xbatcher/__init__.py
+++ xbatcher/__init__.py
@@ -1,11 +1,4 @@
-from pkg_resources import DistributionNotFound, get_distribution
-
+from ._version import __version__  # noqa: F401
 from .accessors import BatchAccessor  # noqa: F401
 from .generators import BatchGenerator  # noqa: F401
 from .util.print_versions import show_versions  # noqa: F401
-
-try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:  # noqa: F401; pragma: no cover
-    # package is not installed
-    pass
---
+++ xbatcher/_version.py
@@ -0,0 +1,7 @@
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    # package is not installed
+    __version__ = 'unknown'
