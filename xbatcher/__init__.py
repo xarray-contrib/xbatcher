25595fbe13902e343734705d6c10bab9fa152285
Raphael Hagen <norlandrhagen@gmail.com>
2022-09-27 09:37:49 -0700

    moved version into __init__.py

--- xbatcher/__init__.py
+++ xbatcher/__init__.py
@@ -1,4 +1,11 @@
-from ._version import __version__  # noqa: F401
+from importlib.metadata import PackageNotFoundError, version
+
 from .accessors import BatchAccessor  # noqa: F401
 from .generators import BatchGenerator  # noqa: F401
 from .util.print_versions import show_versions  # noqa: F401
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    # package is not installed
+    __version__ = 'unknown'
